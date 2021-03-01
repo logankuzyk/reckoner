ee8cf876b54e70e7c7515f841eead5576f5b5d68
Logan Kuzyk <logan@kuzyk.net>
2021-02-28 19:57:21 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+const snake = require("./snake");
+const {
+  fallbackHandler,
+  notFoundHandler,
+  genericErrorHandler,
+  poweredByHandler,
+} = require("./handlers.js");
+
+// For deployment to Heroku, the port needs to be set using ENV, so
+// we check for the port number in process.env
+app.set("port", process.env.PORT || 5000);
+
+app.enable("verbose errors");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(poweredByHandler);
+
+// --- SNAKE LOGIC GOES BELOW THIS LINE ---
+
+// Handle POST request to '/start'
+app.post("/start", (request, response) => {
+  // NOTE: Do something here to start the game
+  return response.status(200).json({});
+});
+
+// Handle POST request to '/move'
+app.post("/move", (request, response) => {
+  // NOTE: Do something here to generate your move
+
+  // Response data
+  const data = {
+    move: snake(request.body), // one of: ['up','down','left','right']
+  };
+
+  return response.status(200).json(data);
+});
+
+app.post("/end", (request, response) => {
+  // NOTE: Any cleanup when a game is complete.
+  return response.json({});
+});
+
+app.get("/", (request, response) => {
+  // Used for checking if this snake is still alive.
+  return response.status(200).json({
+    apiversion: "1",
+    author: "logankuzyk",
+    color: "#FFFF00",
+    head: "default",
+    tail: "sharp",
+  });
+});
+
+// --- SNAKE LOGIC GOES ABOVE THIS LINE ---
+
+app.use("*", fallbackHandler);
+app.use(notFoundHandler);
+app.use(genericErrorHandler);
+
+app.listen(app.get("port"), () => {
+  console.log("Server listening on port %s", app.get("port"));
+});
---
+++ snake.js
@@ -0,0 +1,4 @@
+module.exports = (apiRequest) => {
+  let message = "right";
+  return message;
+};
