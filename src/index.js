e9ba093ba15061d1be31f3d6e129411e22976c85
Logan Kuzyk <logan@kuzyk.net>
2021-03-02 14:34:22 -0800

    Update color and remove comments

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const {
   poweredByHandler,
 } = require('./handlers.js');
 
-// For deployment to Heroku, the port needs to be set using ENV, so
-// we check for the port number in process.env
 app.set('port', process.env.PORT || 5000);
 
 app.enable('verbose errors');
@@ -20,43 +18,31 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(poweredByHandler);
 
-// --- SNAKE LOGIC GOES BELOW THIS LINE ---
-
-// Handle POST request to '/start'
 app.post('/start', (request, response) => {
-  // NOTE: Do something here to start the game
   return response.status(200).json({});
 });
 
-// Handle POST request to '/move'
 app.post('/move', (request, response) => {
-  // NOTE: Do something here to generate your move
-
-  // Response data
   const data = {
-    move: snake(request.body), // one of: ['up','down','left','right']
+    move: snake(request.body),
   };
 
   return response.status(200).json(data);
 });
 
 app.post('/end', (request, response) => {
-  // NOTE: Any cleanup when a game is complete.
   return response.json({});
 });
 
 app.get('/', (request, response) => {
-  // Used for checking if this snake is still alive.
   return response.status(200).json({
     apiversion: '1',
     author: 'logankuzyk',
-    color: '#FFFF00',
+    color: '#61FF7E',
     head: 'default',
     tail: 'sharp',
   });
 });
-
-// --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
 app.use('*', fallbackHandler);
 app.use(notFoundHandler);
