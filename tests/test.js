3193d8df771bca0038c380c4bf3aa3c4acb7569c
Logan Kuzyk <logan@kuzyk.net>
2021-03-04 13:43:58 -0800

    Testing the actual snake now

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,11 @@
-const Board = require("../src/board")
+// const Board = require("../src/board")
 const fs = require("fs")
 
 let request = fs.readFileSync(__dirname + "/request.txt")
 request = JSON.parse(request)
-let board = new Board(request)
+// let board = new Board(request)
+
+const OddOphidian = require("../src/oddOphidian")
+
+let snake = new OddOphidian(request)
+console.log(snake)
