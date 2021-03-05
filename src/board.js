b76d63debe70c15a6df6dd4bda8bb6edb3cf7593
Logan Kuzyk <logan@kuzyk.net>
2021-03-04 17:03:29 -0800

    Store food and add lengthOfPath function

--- src/board.js
+++ src/board.js
@@ -6,6 +6,7 @@ class Board {
     this.grid = this.makeBoard(apiRequest);
     // [snake.id, Snake]
     this.snakes = new Map();
+    this.food = new Map();
     this.loadGamePieces(apiRequest);
   }
   // Coordinate parameter is the same format as a single body element of a snake (for example).
@@ -14,6 +15,14 @@ class Board {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
     return `${alphabet.charAt(coord.x)}${String(coord.y + 1)}`;
+  }
+
+  deleteFood(chess) {
+    let index = this.food.indexOf(chess);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+    this.grid.get(chess).food = false;
   }
 
   isNextToFood(chess) {
@@ -32,6 +41,7 @@ class Board {
     apiRequest.board.food.forEach((food) => {
       let tile = this.grid.get(this.coordToChess(food));
       tile.food = true;
+      this.food.push(this.coordToChess(food));
     });
     apiRequest.board.snakes.forEach((snake) => {
       let snakeObject = new Snake(snake);
@@ -40,6 +50,7 @@ class Board {
       if (this.isNextToFood(this.coordToChess(snake.body[0]))) {
         // 🚨🚨 ASSUMPTION: a snake will eat food if it's directly next to it
         snake.body.unshift(this.isNextToFood(this.coordToChess(snake.body[0])));
+        deleteFood(this.coordToChess(snake.body[0]));
       }
 
       if (snake.body.length < apiRequest.you.body.length) {
@@ -52,8 +63,12 @@ class Board {
         tile.solid = true;
         snakeObject.body.unshift(this.coordToChess(body));
       }
-      if (snake.body[0].x == apiRequest.you.body[0].x && snake.body[0].y == apiRequest.you.body[0].y) {
-        this.snakes.set("me", snakeObject)
+      if (
+        snake.body[0].x == apiRequest.you.body[0].x &&
+        snake.body[0].y == apiRequest.you.body[0].y
+      ) {
+        this.snakes.set('me', snakeObject);
+        this.snakes.get('me').body[0].solid = false;
       } else {
         this.snakes.set(snake.id, snakeObject);
       }
@@ -76,6 +91,44 @@ class Board {
     }
 
     return board;
+  }
+
+  lengthOfPath(chess1, chess2) {
+    let start = this.grid.get(chess1);
+    let end = this.grid.get(chess2);
+    let options = [];
+    let minEval = Infinity;
+
+    if (chess1 == chess2) {
+      return 0;
+    } else if (chess1 == null || chess2 == null || start.solid) {
+      return Infinity;
+    }
+
+    if (end.x > start.x) {
+      // need to go right
+      options.push('right');
+    } else if (end.x < start.x) {
+      // need to go left
+      options.push('left');
+    }
+
+    if (end.y > start.y) {
+      // need to go up
+      options.push('up');
+    } else if (end.y < start.y) {
+      // need to go down
+      options.push('down');
+    }
+
+    for (let move of options) {
+      let length = this.lengthOfPath(start[move], chess2);
+      if (length < minEval) {
+        minEval = length;
+      }
+    }
+
+    return minEval + 1;
   }
 }
 
